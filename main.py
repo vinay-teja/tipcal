35b08d5f21ffd004d158ab13cfc0b9e5b9da486e
VT <vinaytejaa@gmail.com>
2021-09-20 06:48:29 +0000

    to 2 decimal and Currency conversion

--- main.py
+++ main.py
@@ -4,17 +4,20 @@
 
 #
 name = input("Please enter your name! \n")
+
+Currency = input("Please enter your currency!: $ or Rs. or Yen \n")
 print("Welcome to Tip calculator!, " + name)
 
 total_Bill = float(input("what's your total bill? \n"))
 
 party_size = int(input("How many friends are sharing this feast ? \n"))
 
-tip_Percentage = int(input("Choose  your top percentage: Example: 10 or 12 or 15? \n"))
+tip_Percentage = int(
+    input("Choose  your top percentage: Example: 10 or 12 or 15? \n"))
 # final_total = tip_Percentage /100 *total_Bill + total_Bill
 # final_total = total_Bill * (1 + tip_Percentage /100)
 
 final_total = round(
     (total_Bill + (total_Bill * (tip_Percentage / 100))) / party_size, 2)
-
-print(f"Each person pays  {final_total}")
+final_total = "{:.2f}".format(final_total)
+print(f"Each person pays {Currency} {final_total}")
