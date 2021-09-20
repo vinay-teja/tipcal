47b9fc00766a9dc4e7f11a6a97c2edcb0b2fc989
VT <vinaytejaa@gmail.com>
2021-09-20 06:38:45 +0000

    Minor changes and added alternative math  operations. Typos fixed (ofcourse)

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 
-
 #
 name = input("Please enter your name! \n")
-print("Welcomme to Tip calculator, " + name)
+print("Welcome to Tip calculator!, " + name)
 
 total_Bill = float(input("what's your total bill? \n"))
 
-party_size = int(input("How many friends had the share? \n"))
+party_size = int(input("How many friends are sharing this feast ? \n"))
 
-tip_Percentage = int(input("Choose  your top percentage: 10 or 12 or 15? \n"))
+tip_Percentage = int(input("Choose  your top percentage: Example: 10 or 12 or 15? \n"))
+# final_total = tip_Percentage /100 *total_Bill + total_Bill
+# final_total = total_Bill * (1 + tip_Percentage /100)
 
 final_total = round(
     (total_Bill + (total_Bill * (tip_Percentage / 100))) / party_size, 2)
 
-print("Each person pays ")
-print(final_total)
+print(f"Each person pays  {final_total}")
